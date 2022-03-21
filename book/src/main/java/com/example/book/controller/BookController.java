a4f13490940c058cf8d6394a316d1ecef231be3e
Addisalem Wordofa <addaber@gmail.com>
2022-03-20 21:19:52 -0400

    #2

--- book/src/main/java/com/example/book/controller/BookController.java
+++ book/src/main/java/com/example/book/controller/BookController.java
@@ -1,5 +1,16 @@
 package com.example.book.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.book.domain.Book;
+
+@RestController
 public class BookController {
+	
+	@GetMapping("/book")
+	public Book findBook() {
+		return null;
+	}
 
 }
